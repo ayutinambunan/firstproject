b01743e3c454bdf180a60b8619cf8b27c56b7524
ayutinambunan <ayusentiatinambunan@gmail.com>
2023-10-02 08:57:45 +0700

    error

--- app/src/main/java/com/example/praktikummobile/MainActivity.kt
+++ app/src/main/java/com/example/praktikummobile/MainActivity.kt
@@ -7,12 +7,16 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 
+
 class MainActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var input_panjang : EditText
     private lateinit var input_lebar : EditText
     private lateinit var input_tinggi : EditText
     private lateinit var btn : Button
     private lateinit var teks_hasil : TextView
+    companion object {
+        private const val STATE_RESULT =  "state_result"
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -23,15 +27,20 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         teks_hasil= findViewById(R.id.result)
 
         btn.setOnClickListener(this)
-    }
 
-    override fun onClick(v: View?) {
-        if (v?.id==R.id.btn_result){
-            val panjang = input_panjang.text.toString().trim()
-            val lebar = input_lebar.text.toString().trim()
-            val tinggi = input_tinggi.text.toString().trim()
-            val volume = tinggi.toDouble() *panjang.toDouble() *lebar.toDouble()
-            teks_hasil.text = volume.toString()
+        if (savedInstanceState != null){
+            teks_hasil.text = savedInstanceState.getString(STATE_RESULT)
         }
     }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(STATE_RESULT, teks_hasil.text.toString())
+    }
+
+
+
 }
+
+
+
